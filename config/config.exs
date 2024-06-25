60e67ceb165e1cb0ef1d78d5832f8eabb39ad7fd
Dominic Letz <dominic.letz@berlin.de>
2024-06-25 19:31:47 +0200

    updated config

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 # Configures the endpoint
 config :tcpbin, TcpbinWeb.Endpoint,
@@ -18,7 +18,7 @@ config :tcpbin, TcpbinWeb.Endpoint,
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id],
+  metadata: [:request_id]
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
