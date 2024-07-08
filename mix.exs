d581cd9cec1f22a8268222bb5eca54fd9f27f401
Dominic Letz <dominic.letz@berlin.de>
2024-07-08 06:53:39 +0200

    fixup

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Tcpbin.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:certmagex, "~> 1.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
